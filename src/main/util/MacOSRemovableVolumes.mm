d3aa52c53cfab67b4be9e0d4613f1092b7b51b4c
Izmar Verhage <izmaelverhage@gmail.com>
2023-11-12 22:05:36 +0100

    Fix memory leak

--- src/main/util/MacOSRemovableVolumes.mm
+++ src/main/util/MacOSRemovableVolumes.mm
@@ -15,20 +15,28 @@ void RemovableVolumes::diskAppeared(DADiskRef disk, void* context)
     
     if (mediaLeaf.intValue == 1 && ejectable.intValue == 1 && volumeMountable.intValue == 1)
     {
-        NSString* volumeKind = (NSString*) CFDictionaryGetValue(properties, kDADiskDescriptionVolumeKindKey);
+        auto volumeKind = (NSString*) CFDictionaryGetValue(properties, kDADiskDescriptionVolumeKindKey);
         auto volumeKindStr = volumeKind.UTF8String;
         bool isMsDos = strcmp(volumeKindStr, "msdos") == 0;
 
-        if (!isMsDos) return;
+        if (!isMsDos)
+        {
+            CFRelease(properties);
+            return;
+        }
         
-        NSString* volumeType = (NSString*) CFDictionaryGetValue(properties, kDADiskDescriptionVolumeTypeKey);
+        auto volumeType = (NSString*) CFDictionaryGetValue(properties, kDADiskDescriptionVolumeTypeKey);
         auto volumeTypeStr = volumeType.UTF8String;
         bool isFat16 = strcmp(volumeTypeStr, "MS-DOS (FAT16)") == 0;
         
-        if (!isFat16) return;
+        if (!isFat16)
+        {
+            CFRelease(properties);
+            return;
+        }
         
-        NSString* bsdName = (NSString*) CFDictionaryGetValue(properties, kDADiskDescriptionMediaBSDNameKey);
-        CFNumberRef mediaSizeRef = (CFNumberRef) CFDictionaryGetValue(properties, kDADiskDescriptionMediaSizeKey);
+        auto bsdName = (NSString*) CFDictionaryGetValue(properties, kDADiskDescriptionMediaBSDNameKey);
+        auto mediaSizeRef = (CFNumberRef) CFDictionaryGetValue(properties, kDADiskDescriptionMediaSizeKey);
         uint64_t mediaSize;
         CFNumberGetValue(mediaSizeRef, kCFNumberSInt64Type, &mediaSize);
         
@@ -40,7 +48,7 @@ void RemovableVolumes::diskAppeared(DADiskRef disk, void* context)
 
         std::string volumeUUID;
         
-        CFUUIDRef volumeUUIDKey = (CFUUIDRef) CFDictionaryGetValue(properties, kDADiskDescriptionVolumeUUIDKey);
+        auto volumeUUIDKey = (CFUUIDRef) CFDictionaryGetValue(properties, kDADiskDescriptionVolumeUUIDKey);
         if(volumeUUIDKey)
         {
             CFStringRef uuid = CFUUIDCreateString(NULL, volumeUUIDKey);
@@ -58,11 +66,13 @@ void RemovableVolumes::diskAppeared(DADiskRef disk, void* context)
             }
         }
         
-        NSString* volumeName = (NSString*) CFDictionaryGetValue(properties, kDADiskDescriptionVolumeNameKey);
+        auto volumeName = (NSString*) CFDictionaryGetValue(properties, kDADiskDescriptionVolumeNameKey);
         std::string volumeNameStr = volumeName.UTF8String;
         
         for (auto& l : that->listeners)
+        {
             l->processChange(RemovableVolume{volumeUUID, bsdNameStr, volumeNameStr, mediaSize});
+        }
     }
 
     CFRelease(properties);
